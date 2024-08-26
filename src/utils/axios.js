9621fe9ef558e97e665786941f6b23ab6acf3579
cold <kholodihor@gmail.com>
2024-08-26 15:36:09 +0300

    fix: fix endpoint

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL:import.meta.env.VITE_APP_API_URL,
+  baseURL: import.meta.env.VITE_APP_API_URL + "/api",
 });
 
 instance.interceptors.request.use((config) => {
-  config.headers.Authorization = window.localStorage.getItem('token');
+  config.headers.Authorization = window.localStorage.getItem("token");
   return config;
 });
 
