9d4ffa75d9d9456c0496c172cf7de802a3ddcb0b
cold <kholodihor@gmail.com>
2025-12-03 13:09:17 +0100

    fix: minor cleanup

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -12,15 +12,15 @@ instance.interceptors.request.use(config => {
   return config;
 });
 
-instance.interceptors.response.use(
-  response => response,
-  error => {
-    if (error.response?.status === 401) {
-      window.localStorage.removeItem('token');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
+// instance.interceptors.response.use(
+//   response => response,
+//   error => {
+//     if (error.response?.status === 401) {
+//       window.localStorage.removeItem('token');
+//       window.location.href = '/login';
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default instance;
